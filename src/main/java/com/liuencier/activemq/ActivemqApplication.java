19b171086928ac5e7b0d7930c24069ba377b9869
liuenci <154910381@qq.com>
2019-02-21 17:44:54 +0800

    init

---
+++ src/main/java/com/liuencier/activemq/ActivemqApplication.java
@@ -0,0 +1,16 @@
+package com.liuencier.activemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liuenci
+ */
+@SpringBootApplication
+public class ActivemqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivemqApplication.class, args);
+    }
+
+}
